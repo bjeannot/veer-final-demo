0c7b3e8e187cc5592ec35c4cb4749a0207919d1f
Viz Lab <VizLab@stcloudstate.edu>
2019-05-01 18:39:24 -0500

    Deleted many stuff

--- Assets/Scripts/SpeakerIconManager.cs
+++ Assets/Scripts/SpeakerIconManager.cs
@@ -11,40 +11,16 @@
         [Tooltip("The time in seconds to remove the icon completely. Set to -1 to always keep showing the icon")]
         public int InactiveDelay = 3;
         [Tooltip("The voice to listen to")]
-        public PhotonVoiceSpeaker Speaker;
 
         private SpriteRenderer spriteRenderer;
         private float lastTimeTalking;
 
         void Awake() {
-            if (Talking == null) {
-                Debug.LogError("SpeakerIconManager is missing a reference to the talking state sprite!");
-            }
-            if (Speaker == null) {
-                Debug.LogError("SpeakerIconManager is missing a reference to the speaker to listen to!");
-            }
-            spriteRenderer = GetComponent<SpriteRenderer>();
-            if (spriteRenderer == null) {
-                Debug.LogError("SpeakerIconManager requires a SpriteRenderer on the same GameObject!");
-            }
-            if (InactiveDelay == -1) {
-                spriteRenderer.sprite = NotTalking;
-            }
+       
         }
 
         void Update() {
-            if (Speaker.IsPlaying) {
-                spriteRenderer.sprite = Talking;
-                lastTimeTalking = Time.time;
-            } else {
-                if ((Time.time - lastTimeTalking) > SwitchBackDelay) {
-                    if (InactiveDelay != -1 && (Time.time - lastTimeTalking) > (SwitchBackDelay + InactiveDelay)) {
-                        spriteRenderer.sprite = null;
-                    } else {
-                        spriteRenderer.sprite = NotTalking;
-                    }
-                }
-            }
+         
         }
     }
 }
